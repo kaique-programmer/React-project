3503a702315f501394b3a80728afa7186b754dc8
kaique oliveira santos <kaiquevicio@gmail.com>
2021-10-21 18:52:50 -0300

    settings theme

--- landing-pages-strapi-mongodb/landing-pages-react/src/styles/theme.js
+++ landing-pages-strapi-mongodb/landing-pages-react/src/styles/theme.js
@@ -1,10 +1,39 @@
 const theme = {
   colors: {
-    primaryBackground: 'red',
-    secondaryBackground: 'blue',
+    primaryBackground: '#0A1128',
+    secondaryBackground: '#dc143c',
+    white: '#FFF',
+    mediumGray: '#DDD',
   },
-  fonts: {},
-  spacings: {},
+  fonts: {
+    family: {
+      default: "'Opens Sans', sans-serif",
+      secondary: "'Montserrat', sans-serif",
+    },
+    sizes: {
+      xsmall: '8rem',
+      small: '1.6rem',
+      medium: '2.4rem',
+      large: '3.2rem',
+      xlarge: '4.0rem',
+      xxlarge: '4.8rem',
+      huge: '5.6rem',
+      xhuge: '6.4rem',
+    },
+  },
+  media: {
+    lteMedium: '(max-width: 768px)',
+  },
+  spacings: {
+    xsmall: '8rem',
+    small: '1.6rem',
+    medium: '2.4rem',
+    large: '3.2rem',
+    xlarge: '4.0rem',
+    xxlarge: '4.8rem',
+    huge: '5.6rem',
+    xhuge: '6.4rem',
+  },
 };
 
 export default theme;
