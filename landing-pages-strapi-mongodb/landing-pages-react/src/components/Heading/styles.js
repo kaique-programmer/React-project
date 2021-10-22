cd7f1046884fd7b17d23f2d53cde72a3ca1d871e
kaique oliveira santos <kaiquevicio@gmail.com>
2021-10-22 01:23:28 -0300

    settings storybook complete

--- landing-pages-strapi-mongodb/landing-pages-react/src/components/Heading/styles.js
+++ landing-pages-strapi-mongodb/landing-pages-react/src/components/Heading/styles.js
@@ -1,7 +1,7 @@
 import styled, { css } from 'styled-components';
 
 export const Title = styled.h1`
-  ${({ theme }) => css`
-    color: ${light ? theme.colors.white : theme.colors.primaryBackground }
+  ${({ theme, light }) => css`
+    color: ${ light ? theme.colors.white : theme.colors.primaryBackground }
   `}
 `;
