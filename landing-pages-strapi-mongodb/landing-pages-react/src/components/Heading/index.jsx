19af5b50f60a63cdadaf683b36cf4f531f22b820
kaique oliveira santos <kaiquevicio@gmail.com>
2021-10-22 01:27:13 -0300

    settings storybook complete

--- landing-pages-strapi-mongodb/landing-pages-react/src/components/Heading/index.jsx
+++ landing-pages-strapi-mongodb/landing-pages-react/src/components/Heading/index.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Props from 'props-types';
+import Props from 'prop-types';
 import * as Styled from './styles';
 
 const Heading = ({ children, light = false }) => {
