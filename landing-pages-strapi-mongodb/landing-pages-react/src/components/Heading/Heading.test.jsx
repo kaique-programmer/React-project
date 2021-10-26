2b9a9dbc26faf4893969961c4acdb5e0123ce877
kaique oliveira santos <kaiquevicio@gmail.com>
2021-10-25 23:50:31 -0300

    test heading

---
+++ landing-pages-strapi-mongodb/landing-pages-react/src/components/Heading/Heading.test.jsx
@@ -0,0 +1,67 @@
+import React from 'react';
+import { screen } from '@testing-library/react';
+import { ThemeProvider } from 'styled-components';
+import Heading from '.';
+import { renderTheme } from '../../styles/render-theme';
+import theme from '../../styles/theme';
+
+describe('<Heading />', () => {
+  it('should render with values', () => {
+    renderTheme(<Heading>text</Heading>);
+    const heading = screen.getByRole('heading', { name: 'text' });
+
+    expect(heading).toHaveStyle({
+      color: theme.colors.primaryBackground,
+      'font-size': theme.font.sizes.xhuge,
+      'text-transform': 'none',
+    });
+  });
+
+  it('should render with white color', () => {
+    renderTheme(<Heading colorDark={false}>text</Heading>);
+    const heading = screen.getByRole('heading', { name: 'text' });
+
+    expect(heading).toHaveStyle({
+      color: theme.colors.white,
+    });
+  });
+
+  it('should render correct heading sizes', () => {
+    renderTheme(<Heading size="small">text</Heading>);
+    const heading = screen.getByRole('heading', { name: 'text' });
+
+    expect(heading).toHaveStyle({
+      'font-size': theme.font.sizes.medium,
+    });
+  });
+
+  it('should render correct font-size when using mobile', () => {
+    renderTheme(<Heading size="huge">text</Heading>);
+    const heading = screen.getByRole('heading', { name: 'text' });
+
+    expect(screen.getByRole('heading', { name: 'text' })).toHaveStyleRule(
+      'font-size',
+      theme.font.sizes.xlarge,
+      {
+        media: theme.media.lteMedium,
+      },
+    );
+  });
+
+  it('should render with uppercase letters', () => {
+    renderTheme(<Heading uppercase={true}>text</Heading>);
+    const heading = screen.getByRole('heading', { name: 'text' });
+
+    expect(heading).toHaveStyle({
+      'text-transform': 'uppercase',
+    });
+  });
+
+  it('should render correct heading element', () => {
+    const { container } = renderTheme(<Heading as="h6">text</Heading>);
+    const heading = screen.getByRole('heading', { name: 'text' });
+    const h6 = container.querySelector('h6');
+
+    expect(h6.tagName.toLowerCase()).toBe('h6');
+  });
+});
--- landing-pages-strapi-mongodb/landing-pages-react/src/templates/App/App.test.jsx
+++ landing-pages-strapi-mongodb/landing-pages-react/src/templates/App/App.test.jsx
@@ -1,14 +1,9 @@
 import React from 'react';
 import { renderTheme } from '../../styles/render-theme';
-import { screen } from '@testing-library/dom';
 import Home from '.';
-import theme from '../../styles/theme';
 
-test('renders learn react link', () => {
-  const { debug } = renderTheme(<Home />);
-  const headingContainer = screen.getByRole('heading', { name: 'Hello' }).parentElement;
-  expect(headingContainer).toHaveStyle({
-    background: theme.colors.secondaryBackground,
+describe('<Home />', () => {
+  it('should render home', () => {
+    renderTheme(<Home />);
   });
-  expect(headingContainer).toHaveStyleRule('background', 'red');
 });
