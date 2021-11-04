5c01911dc0a48562b3b33a3ab85a3425496fc463
kaique oliveira santos <kaiquevicio@gmail.com>
2021-11-04 19:47:13 -0300

    initial tests

--- landing-pages-strapi-mongodb/landing-pages-react/src/components/Menu/Menu.test.jsx
+++ landing-pages-strapi-mongodb/landing-pages-react/src/components/Menu/Menu.test.jsx
@@ -3,9 +3,16 @@ import { screen } from '@testing-library/react';
 import { renderTheme } from '../../styles/render-theme';
 import { Menu } from '.';
 
+import linksMock from '../NavLinks/mock';
+
+const logoData = {
+  text: 'Logo',
+  link: '#target',
+}
+
 describe('<Menu />', () => {
   it('should render', () => {
-    renderTheme(<Menu>Children</Menu>);
-    expect(screen.getByRole('heading')).toBeInTheDocument();
+    renderTheme(<Menu links={linkMock} logoData={logoData} />);
+    expect(screen.getByRole('heading', { name: 'Logo' })).toBeInTheDocument();
   });
 });
