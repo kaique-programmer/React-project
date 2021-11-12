f5e25f82cccfde97c697ba8fcbe41e4d14565e70
kaique oliveira santos <kaiquevicio@gmail.com>
2021-11-11 22:42:45 -0300

    component template base - part 1

---
+++ landing-pages-strapi-mongodb/landing-pages-react/src/templates/Base/Base.test.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { screen } from '@testing-library/react';
+import { renderTheme } from '../../styles/render-theme';
+import { Base } from '.';
+
+describe('<Base />', () => {
+  it('should render', () => {
+    renderTheme(<Base>Children</Base>);
+    expect(screen.getByRole('heading')).toBeInTheDocument();
+  });
+});
---
+++ landing-pages-strapi-mongodb/landing-pages-react/src/templates/Base/index.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import Props from 'prop-types';
+import * as Styled from './styles';
+import { Menu } from '../../components/Menu';
+import { Footer } from '../../components/Footer';
+import { GoTop } from '../../components/GoTop';
+
+export const Base = ({ links = [], logoData, footerHtml, children }) => {
+  return (
+    <>
+      <Menu links={links} logoData={logoData} />
+      <Styled.Container>
+        {children}
+        <Footer html={footerHtml} />
+      </Styled.Container>
+      <GoTop />
+    </>
+  );
+};
+
+Base.propTypes = {
+  children: Props.node.isRequired,
+  ...Menu.propTypes,
+  footerHtml: Props.string.isRequired,
+};
