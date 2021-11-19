69dc26495d742b9bb9cfdd1d6708745fc215e86a
kaique oliveira santos <kaiquevicio@gmail.com>
2021-11-19 18:04:43 -0300

    clear test

--- landing-pages-strapi-mongodb/landing-pages-react/src/templates/App/App.test.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { renderTheme } from '../../styles/render-theme';
-import Home from '.';
-
-describe('<Home />', () => {
-  it('should render home', () => {
-    renderTheme(<Home />);
-  });
-});
